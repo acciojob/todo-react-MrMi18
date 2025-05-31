22989c73066dce704c3174f6772ed9def683bc3d
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-05-31 11:18:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
-import React from "react";
+import React, { useState } from 'react'
 import './../styles/App.css';
 
 const App = () => {
+ const [ todo, setTodo] = useState("");
+  const[list , setList] = useState([])
+  const addTodoHandler = () =>{
+    setList([...list,todo])
+    setTodo("");
+  }
+  const deleteTodoHandler = (td) =>{
+    setList(list.filter(t => td!==t));
+  }
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>To-Do List </h1>
+        <input type='text' value={todo}
+        onChange={(e)=>setTodo(e.target.value)}
+        ></input>
+        <button onClick={addTodoHandler}>Add Todo</button>
+     <ul>
+      {
+        list.length>0 && 
+        list.map(td => {
+          return <li key={td}> {td} 
+            <button onClick={()=>deleteTodoHandler(td)}>Delete</button>
+          </li>
+        })
+        
+      }
+     </ul>
+
     </div>
   )
 }
